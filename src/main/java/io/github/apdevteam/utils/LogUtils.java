c718dde20502139c95e4fe0caa50b1d91bf7302f
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-08-17 19:38:25 -0500

    Create LogUtils.java

---
+++ src/main/java/io/github/apdevteam/utils/LogUtils.java
@@ -0,0 +1,25 @@
+package io.github.apdevteam.utils;
+
+import java.io.File;
+
+public class LogUtils {
+    public static boolean exists(String userID) {
+        return false;
+    }
+
+    public static void create(String userID) {
+
+    }
+
+    public static void log(String userID, String message) {
+
+    }
+
+    public static File retrieve(String userID) {
+        return null;
+    }
+
+    public static void delete(String userID) {
+
+    }
+}
