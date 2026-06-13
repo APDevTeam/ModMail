cc87d3734b774badb93e0db09196dc4f47838fd6
TylerS1066 <TylerS1066@users.noreply.github.com>
2026-06-13 13:14:50 -0500

    Fix build and CI

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Discord bot to enable players to message staff collectively through the usage of a bot account.
 
-This bot is written in Java using the [Java Discord API (JDA)](https://github.com/DV8FromTheWorld/JDA).
+This bot is written in Java using the [Java Discord API (JDA)](https://github.com/discord-jda/JDA).
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 repositories {
@@ -21,6 +22,16 @@ description = "ModMail"
 java.toolchain.languageVersion = JavaLanguageVersion.of(21)
 
 tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "io.github.apdevteam.ModMail",
+            "Implementation-Version" to project.version,
+            "Implementation-Title" to project.name
+        )
+    }
+}
+
+tasks.shadowJar {
     archiveBaseName.set("ModMail")
     archiveClassifier.set("")
     archiveVersion.set("")
